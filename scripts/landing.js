6ffce7c1f4fd97f07bab7b2e5d5c4f6ea678bde6
Jay <jamescarlson2@hotmail.com>
2016-03-15 11:54:21 -0500

    Add jQuery to index animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,23 @@
-var points = document.getElementsByClassName('point');
+var animatePoints = function(){
+  var revealPoint = function() {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+  $.each($('point'), revealPoint);
+};
 
-function revealPoint(points) {
-    var i = 0;
-    for (i; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-}
-
-window.onload = function() {
-  if (window.innerHeight > 950) {
-    revealPoint(points);
+$(window).load(function() {
+  if ($(window).height() > 950) {
+    animatePoints();
   }
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-      revealPoint(points);
+  $(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
   });
-}
+})
